a7c52965329173609999a2dc887c999b86874167
ViniCampos12 <viniciuspriamo@gmail.com>
2025-10-28 17:32:13 -0300

    Adiciona comentários

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     { link: "https://github.com/ViniCampos12", icon: "/images/phone.png" },
   ];
 
-  const descriptionsCard = ["Tornar a terapia um espaço acessível, leve e transformador, promovendo o autoconhecimento, o bem-estar e equilíbrio emocional.", "Ser referência em atendimento psicológico humanizado, conectando pessoas ao seu próprio processo de desenvolvimento.", "Empatia Autenticidade Cuidado Evolução Individualidade Respeito Sensibilidade"];
+  const descriptionsCard = ["Tornar a terapia um espaço acessível, leve e transformador, promovendo o autoconhecimento, o bem-estar e equilíbrio emocional.", "Ser referência em atendimento psicológico humanizado, conectando pessoas ao seu próprio processo de desenvolvimento.", "Empatia, Autenticidade, Cuidado, Evolução, Individualidade, Respeito, Sensibilidade"];
 
   return (
     <main>
@@ -52,6 +52,8 @@ export default function Home() {
         style_card={{backgroundColor:"#B54A22",color:"white"}}
       />
       
+
+      {/* Parte da página principal que vocês deverão utilizar para adicionar informações vindas do banco de dados */}
       <ServicesThree
         title_card={["Lorem Ipsum","Lorem Ipsum","Lorem Ipsum","Lorem Ipsum","Lorem Ipsum","Lorem Ipsum",]}
         description_card={["Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est modi quaerat sint tenetur aliquam","Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est modi quaerat sint tenetur aliquam","Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est modi quaerat sint tenetur aliquam","Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est modi quaerat sint tenetur aliquam","Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est modi quaerat sint tenetur aliquam","Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est modi quaerat sint tenetur aliquam"]}
@@ -61,11 +63,13 @@ export default function Home() {
         style_card={{backgroundColor:"#B54A22", color:"white"}}
         text_style={{color:"#B54A22"}}
       />
+      {/*  */}
+
 
       <FeedbackOne
         style={{backgroundColor:"white", color:"#B54A22"}}
         client_names={["Júlia", "Vinicius"]}
-        companies={["",""]}
+        companies={["Psicode","Psicode"]}
         descriptions={["“Gostaria de parabenizar o atendimento humanizado e individual. Particularmente, enxergo a grande diferença em não ter contato robotizado em passar valores ou tirar dúvidas etc, pois atendem a necessidade de cada um. Estou amando muito a atenção da psicóloga. Tem feito com que eu reflita sobre os assuntos discutidos e com que me sinta acolhida.”", "“Gostaria de parabenizar vocês pelo atendimento humanizado e individual. Particularmente, enxergo a grande diferença em não ter contato robotizado em passar valores ou tirar dúvidas etc, pois atendem a necessidade de cada um. Estou amando muito a atenção da psicóloga. Tem feito com que eu reflita sobre os assuntos discutidos e com que me sinta acolhida.”"]}
         job_titles={["Paciente","Paciente"]}
         card_style={{backgroundColor:"#F7EDE1"}}
