defc866d456756bf095e135001b2533c524406c6
Ana Carolina Junqueira e Silva <anacarolina.junqueira@estudante.ufjf.br>
2025-11-21 17:49:31 -0300

    arrumando a responsividade do botão do whatsapp

--- src/app/componentesProntos/detalhes_servico/botao_whatsapp/index.tsx
+++ src/app/componentesProntos/detalhes_servico/botao_whatsapp/index.tsx
@@ -12,8 +12,7 @@ export default function WhatsAppButton({phone, message}: WhatsAppButtonProps){
         <Link
         href={link}
         target="_blank"
-        className="inline-block bg-[#B54A22] text-white font-medium py-3 px-8 rounded-xl hover:bg-[#7b3f2f] transition-all"
-        >
+        className="inline-flex items-center justify-center bg-[#B54A22] text-white font-medium py-3 px-8 rounded-xl hover:bg-[#7b3f2f] transition-all text-center">
             Agendar consulta pelo WhatsApp
         </Link>
     )
